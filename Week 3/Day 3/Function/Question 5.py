69a9c4c94d2ca52d5108d64ea9bc209fe2767aee
Chaitanya Neware <87620643+ChaitanyaNeware@users.noreply.github.com>
2022-11-01 22:31:57 +0530

    Add files via upload

--- Week 3/Day 3/Function/Question 5.py
+++ Week 3/Day 3/Function/Question 5.py
@@ -1,20 +1,5 @@
-def returnSum(myDict):
- 
-
-    list = []
-
-    for i in myDict:
-
-        list.append(myDict[i])
-
-    final = sum(list)
- 
-
-    return final
- 
- 
-# Driver Function
-
-dict = ("Enter:")
-
-print("Sum :", returnSum(dict))
+a = int(input("Enter the first item price: "))
+b = int(input("Enter the second item price: "))
+c = int(input("Enter the third item price: "))
+total = a+b+c
+print("Total marks: ",total)
